5eee5a3d811ab34922bcd82579dce9d3c71efb32
Matt Sirkis <sirkis.matt@gmail.com>
2020-06-19 14:49:35 +1200

    updated js and html

--- index.js
+++ index.js
@@ -5,10 +5,18 @@ function getDogImage() {
     let answer = document.getElementById("breed").value;
     //console.log(answer)
     fetch(`https://dog.ceo/api/breed/${answer}/images/random`)
+    .then((response) => { if(!response.ok) {displayError();} 
+    return response;})
     .then(response => response.json())
     .then(responseJson => 
       displayResults(responseJson))
-    .catch(error => console.log('Something went wrong. Try again later.'));
+    .catch(error => console.log('Something went wrong. Try again later.'))
+}
+
+function displayError() {
+  console.log("Sorry, we don't have a picture of that breed!")
+  
+  $('.altBox').html(`<section class="results hidden"><h2>You're barking up the wrong tree!</h2><p>We don't have a picture of that breed.</section><div class="results-img"></div>`)
 }
 
 function displayResults(responseJson) {
@@ -18,13 +26,14 @@ function displayResults(responseJson) {
     `<img src="${responseJson.message}" class="results-img">`
   )
   //display the results section
-  $('.results').removeClass('hidden');
-}
+  $('.results').removeClass('hidden');}
+
 
 
 function watchForm() {
   $('form').submit(event => {
     event.preventDefault();
+    //noBreed();
     getDogImage();
   });
 }
